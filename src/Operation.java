99864a52e10bc9c3c67bea1909a8eb5fdb409f15
burak <burak.cbdn@gmail.com>
2020-04-26 15:24:35 +0300

    Operation class added.

---
+++ src/Operation.java
@@ -0,0 +1,18 @@
+public class Operation {
+
+    private String operator;
+    private String operation;
+
+    public Operation(String opr , String opn) {
+        operator = opr;
+        operation = opn;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+}
